5a780598e2e26c699fc825a9934d3027c4df2367
goncalo <73084553+goncalofso@users.noreply.github.com>
2022-05-13 18:22:11 +0100

    Update StateMachine.c

--- src/Statemachine.c
+++ src/Statemachine.c
@@ -5,21 +5,6 @@
  * @bug No known bugs.
  */
 
-//estados
-#define inicio 0
-#define beer 1
-#define tuna 2
-#define select_e 3
-#define inserir 4
-#define retorno 5
-//acoes/flags
-#define coffee 6
-#define up 1
-#define down 2
-#define select 4
-#define bretorno 3
-#define moeda 5
-
 #include <zephyr.h>
 #include <device.h>
 #include <devicetree.h>
